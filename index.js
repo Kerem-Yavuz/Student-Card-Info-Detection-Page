a5bd7755927cbb31be8c9f5aa2ec818f0f854347
Kerem-Yavuz <kerem.yavuz077@gmail.com>
2024-08-08 14:54:05 +0300

    Added Functionality to Sign Up Page
    included node modules so i dont have to deal with removing them

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require("express");
 const crypto = require('crypto');
 var app = express();
 var path = require('path');
+var session = require('express-session');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,"/public")));
@@ -21,12 +22,56 @@ con.connect(function(err) {
         console.log("Connected to MySQL database!");
     });
 
+    app.use(session({
+        secret: 'asdfhgdsoim',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false } // Set to true if using HTTPS
+      }));
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    function isAuthenticated(req, res, next) {
+        if (req.session.loggedin) {
+          return next();
+        } else {
+          res.redirect('/login'); // Redirect to login if not authenticated
+        }
+      }
+
+      function isHavePriv(req, res, next) {
+        let username = req.session.username;
+        let grantee = `'${username}'@'localhost'`; // Construct the GRANTEE value in JavaScript
+    
+        let query = `
+            SELECT PRIVILEGE_TYPE 
+            FROM information_schema.SCHEMA_PRIVILEGES 
+            WHERE GRANTEE = ? 
+            AND PRIVILEGE_TYPE = 'SELECT' 
+            AND TABLE_SCHEMA = 'webfinal'
+        `;
+    
+        con.query(query, [grantee], (error, results) => {
+            if (error) {
+                console.error('Database query error priv:', error); // Log error details
+                return res.status(500).send('Database query error priv');
+            }
+    
+            if (results.length > 0) { // Fixed typo: `lenght` to `length`
+                return next();
+            } else {
+                return res.status(403).send('Dont have Privilages');
+            }
+        });
+    }
+
 
     app.get("/anasayfa", (req,res) => {
         res.render("main");
     });
 
-    app.get("/table", (req,res) => { //gets all values from data for /anasayfa
+    app.get("/table", isAuthenticated, isHavePriv,  (req,res) => { //gets all values from data for /anasayfa
         let  query = 'SELECT id, sehir_adi FROM sehirler';
         con.query(query, function (err, datas) {
             res.render('DB', { data: datas });
@@ -35,7 +80,7 @@ con.connect(function(err) {
 
     
 
-    app.get("/table/arama", (req, res) => { // gets values for given queries
+    app.get("/table/arama",isAuthenticated, isHavePriv, (req, res) => { // gets values for given queries
         const nesne = {
             kosul: req.query.kosul,
             aramaturu: req.query.aramaturu,
@@ -77,19 +122,22 @@ con.connect(function(err) {
             password: req.query.password
         };
         let hashedPassword = hashPassword(person.password);
-        console.log(hashedPassword);
+        
         let query = 'SELECT authentication_string FROM mysql.user WHERE user = ?;';
         let name = [person.username];
-    
+        
         con.query(query, name, function (err, results) {
             if (err) {
                 return res.status(500).send('Database query failed.');
             }
-            console.log(results);
+           
             if (results.length > 0) {
                 let storedHashedPassword = results[0].authentication_string;
+                console.log(storedHashedPassword);
+                console.log(hashedPassword);
                 if (storedHashedPassword === hashedPassword) {
-                    // do something if the passwords match
+                    req.session.loggedin = true;
+                    req.session.username = person.username; //passwords matched so changes session
                     res.redirect("/table");
                 } else {
                     res.send('Authentication failed.');
@@ -111,10 +159,46 @@ con.connect(function(err) {
         const person =
         {
             username: req.query.username,
-            password: req.query.username
+            password: req.query.password
         }
+        let usercheck = 'SELECT authentication_string FROM mysql.user WHERE user = ?;';
+        let name = [person.username];
+
+        con.query(usercheck, name, function (err, results) {
+            if (err) {
+                return res.status(500).send('Database query failed.');
+            }
+            
+            if (results.length > 0) {
+                res.send("you already have account");
+            } else {
+                
+
+                let createUserQuery = `CREATE USER '${person.username}'@'localhost' IDENTIFIED  WITH mysql_native_password BY '${person.password}';`;//creates user with mysql native password
+                let grantPrivilegesQuery = `GRANT SELECT ON webfinal.* TO '${person.username}'@'localhost';`;//gives them a select privilage on webfinal
+                
+
+                con.query(createUserQuery, function (err, result) {
+                    if (err) {
+                        console.error("Failed to create user:", err);
+                        return res.status(500).send("Failed to create user");
+                    }
+                
+                    con.query(grantPrivilegesQuery, function (err, result) {
+                        if (err) {
+                            console.error("Failed to grant privileges:", err);
+                            return res.status(500).send("Failed to grant privileges");
+                        }
+                
+                        res.redirect("/login");
+                    });
+                });
+
+            }
+        });
         
     });
+   
 
 
     app.get('/', (req, res) => {
